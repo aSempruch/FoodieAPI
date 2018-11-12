e65672abd0c0273e493433178562c27db443185a
aSempruch <32146438+aSempruch@users.noreply.github.com>
2018-11-12 17:17:52 -0500

    Address geocode retrieval done

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -1,8 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var fetch = require('node-fetch');
+var keys = require('../keys');
+
+function getGIO(address, cb) {
+    fetch(`https://api.geocod.io/v1.3/geocode?api_key=${keys.geocodio}&q=${address}`)
+        .then(res => res.json())
+        .then(res => cb(false, res))
+        .catch(err => cb(err));
+}
 
 router.get('/', function(req, res, next) {
-  res.send('respond with a list of restaurants');
+  var { address } = req.query;
+
+  if(!address) res.send("No address provided");
+
+  getGIO(address, (err, data) => {
+    if(err) return res.send("Error getting geocode: " + err);
+      res.send(data);
+  });
 });
 
 module.exports = router;
