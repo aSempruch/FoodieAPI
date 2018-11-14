67744dff0b7206bd83e4799d74a2830fe5726f78
aSempruch <32146438+aSempruch@users.noreply.github.com>
2018-11-13 20:40:34 -0500

    Zomato fetching done

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -10,14 +10,28 @@ function getGIO(address, cb) {
         .catch(err => cb(err));
 }
 
+function getRestaurants(location, cb) {
+    fetch(`https://developers.zomato.com/api/v2.1/geocode?user-key=${keys.zomato}&lat=${location.lat}&lon=${location.lng}`, {
+        headers: {
+            'user-key': keys.zomato
+        }
+    })
+        .then(res => res.json())
+        .then(res => cb(false, res))
+        .catch(err => cb(err));
+}
+
 router.get('/', function(req, res, next) {
   var { address } = req.query;
 
   if(!address) res.send("No address provided");
 
   getGIO(address, (err, data) => {
-    if(err) return res.send("Error getting geocode: " + err);
-      res.send(data);
+      if(err) return res.send("Error getting geocode: " + err);
+      getRestaurants(data.results[0].location, (err, data) => {
+          if(err) return res.send("Error retrieving restaurants " + err);
+          res.send(data);
+      });
   });
 });
 
